acb3987d1993fb0b5ec3e0707613ba81b1201fdb
ValRaz <valerie.dres@gmail.com>
2025-04-07 16:43:59 -0600

    runtime error addressed in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,36 +1,28 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SacramentMeetingPlanner.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ✅ Add services to the container BEFORE calling builder.Build()
 builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-//Adds services to the container
-builder.Services.AddRazorPages();
-
-//Registers AppDbContext with SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SacramentMeetingDB")));
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapStaticAssets(); // if using static assets like CSS/JS/images
+app.MapRazorPages().WithStaticAssets();
 
 app.Run();
